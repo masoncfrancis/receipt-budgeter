c04af66fd9d26ff74f50a8d2e21cd1b14823a422
Mason Francis <mason@MF-Air.localdomain>
2025-11-22 23:45:59 -0500

    Removed old config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { tr } from 'zod/locales'
 
 // https://vite.dev/config/
 export default defineConfig({
